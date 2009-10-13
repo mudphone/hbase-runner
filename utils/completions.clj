be5bf74780181543c24686b11cf8ea6a7cba5ded
Kyle Oba <koba@mudphone.com>
2009-10-12 19:20:21 -0700

    updated ns for hbase-repl

--- utils/completions.clj
+++ utils/completions.clj
@@ -1,7 +1,7 @@
-(use 'mudphone.hbase-runner.repl-cheat)
+(use 'mudphone.hbase-runner.hbase-repl)
 (def completions
   (reduce concat (map (fn [p] (keys (ns-publics (find-ns p))))
-                      '(clojure.core clojure.set clojure.xml clojure.zip mudphone.hbase-runner.repl-cheat))))
+                      '(clojure.core clojure.set clojure.xml clojure.zip mudphone.hbase-runner.hbase-repl))))
 
 (with-open [f (java.io.BufferedWriter. (java.io.FileWriter. (str (System/getenv "HOME") "/.clj_completions")))]
   (.write f (apply str (interleave completions (repeat "\n")))))
