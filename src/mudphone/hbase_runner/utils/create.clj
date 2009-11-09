dff6842e8b9efdd34d0ec4672d5b4212735c0ef5
Kyle Oba <koba@mudphone.com>
2009-11-08 18:57:41 -0800

    remove unnecessary function

--- src/mudphone/hbase_runner/utils/create.clj
+++ src/mudphone/hbase_runner/utils/create.clj
@@ -3,9 +3,6 @@
   (:use mudphone.hbase-runner.utils.clojure)
   (:use mudphone.hbase-runner.utils.file))
 
-(defn- table-descriptor-for [htable]
-  (.getTableDescriptor htable))
-
 (defn- compression-name-for [hcolumn-descriptor]
   (.getName (.getCompression hcolumn-descriptor)))
 
@@ -19,15 +16,15 @@
    :block-cache-enabled (.isBlockCacheEnabled hcolumn-descriptor)})
 
 (defn table-map-for [htable]
-  (let [table-descriptor (table-descriptor-for htable)
+  (let [table-descriptor (.getTableDescriptor htable)
         hcolumn-descriptors (.getFamilies table-descriptor)
         table-map {:name (.getNameAsString table-descriptor)}
         family-maps (map #(family-map-from-hcolumn-descriptor %) hcolumn-descriptors )]
     (assoc table-map :families (into-array family-maps))))
 
-(defn dump-table-to-ruby [output-dir table-name]
+(defn dump-table-to-ruby [output-dir htable]
   (let [file (str output-dir "/tables.rb")]
-    (spit file (str (table-descriptor-for table-name)))))
+    (spit file (str (.getTableDescriptor htable)))))
                                                                             
 (defn dump-tables-to-ruby [output-dir table-names]
   (map #(dump-table-to-ruby output-dir %) table-names))
