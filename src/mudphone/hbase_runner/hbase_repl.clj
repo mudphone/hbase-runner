8e7edf1a9840fd0232abd73a4cecf6fb6470c9e3
Kyle Oba <koba@mudphone.com>
2009-11-12 02:04:51 -0800

    add delimiters to first/end key display

--- src/mudphone/hbase_runner/hbase_repl.clj
+++ src/mudphone/hbase_runner/hbase_repl.clj
@@ -173,6 +173,6 @@
         start-keys (map #(String. %) (.getFirst start-end-byte-arrays))
         end-keys (map #(String. %) (.getSecond start-end-byte-arrays))]
     (println "first start-key" (first start-keys))
-    (println "all start-keys" (str-utils/str-join " " start-keys))
+    (println "all start-keys |" (str-utils/str-join "-" start-keys) "|")
     (println "first end-key" (first end-keys))
-    (println "all end-keys" (str-utils/str-join " " end-keys))))
+    (println "all end-keys |" (str-utils/str-join "-" end-keys) "|")))
