0e8d640a7a1fa4478ea93dd15df4a6d39b06fa0a
Kyle Oba <koba@mudphone.com>
2010-01-29 22:14:17 -0800

    fix double-nesting of output rows

--- src/hbase_runner/hbase/result.clj
+++ src/hbase_runner/hbase/result.clj
@@ -18,21 +18,6 @@
   (or (and limit (> limit 0) (take limit results))
       results))
 
-;; (defn- stringify-version-entry [v-entry]
-;;   {(.getKey v-entry) (String. (.getValue v-entry))})
-
-;; (defn- stringify-column-entry [c-entry]
-;;   {(String. (.getKey c-entry)) (apply merge (map stringify-version-entry (.getValue c-entry)))})
-
-;; (defn- stringify-qualifier-entry [q-entry]
-;;   {(String. (.getKey q-entry)) (apply merge (map stringify-column-entry (.getValue q-entry)))})
-
-;; (defn stringify-nav-map [nmap]
-;;   (apply merge (map stringify-qualifier-entry nmap)))
-
-;; (defn all-result-cols-to-map [result]
-;;   (stringify-nav-map (.getMap result)))
-
 (defn row-id-from-kv [kv]
   (String. (.getRow kv)))
 
@@ -51,8 +36,6 @@
 (defn merge-kv-value [row-map kv]
   (let [map-keys [(family-from-kv kv) (qualifier-from-kv kv) (timestamp-from-kv kv)]
         aggregate-value #(conj (or %1 []) %2)]
-    ;; (println "row-map:" row-map)
-    ;; (println "value is:" (timestamp-from-kv kv) (value-from-kv kv))
     (update-in row-map map-keys aggregate-value (value-from-kv kv))))
 
 (defn- collect-from-kv [map-bucket kv]
@@ -64,8 +47,7 @@
 
 (defn print-scan [results]
   (doseq [result results]
-    (pprint {(row-id-for result)
-             (result-to-map result)})))
+    (pprint (result-to-map result))))
 
 (defn results-to-map [results {:keys [limit print-only]}]
   (let [limited-results (limit-results limit results)]
@@ -73,12 +55,3 @@
       (print-scan limited-results)
       (map result-to-map limited-results)))
   )
-
-;; (defn results-to-map [results {:keys [limit print-only]}]
-;;   (let [limited-results (limit-results limit results)]
-;;     (if print-only
-;;       (print-scan limited-results)
-;;       (reduce #(assoc %1
-;;                  (row-id-for %2) (all-result-cols-to-map %2))
-;;               {} (reverse limited-results))))
-;;   )
